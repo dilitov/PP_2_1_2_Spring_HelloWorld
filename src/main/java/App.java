641480991c2aa1bede10ef5e98bd3fcaac31bc9a
Dilitov <tfutka@gmail.com>
2022-10-20 17:34:43 +0300

    ver.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat one =
+                (Cat) applicationContext.getBean("cat");
+        Cat two =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean1);
+        System.out.println(one == two);
     }
 }
